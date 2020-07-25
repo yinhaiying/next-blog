baada5d1b47e50609e5c49dfae08089981b67f31
yinhaiying <18840842571@163.com>
2020-07-25 08:54:00 +0800

    MVC模式：将涉及数据校验的部分放入Model层即entity中

--- pages/api/v1/users.tsx
+++ pages/api/v1/users.tsx
@@ -5,41 +5,19 @@ import md5 from 'md5'
 const Posts: NextApiHandler = async (req, res) => {
   const { username, password, passwordConfirmation } = req.body;
   const connection = await getDatabaseConnection();
-  // 首先校验数据
-  const errors = {
-    username: [] as string[],
-    password: [] as string[],
-    passwordConfirmation: [] as string[]
-  }
-  if (username.trim().length === 0) {
-    errors.username.push('用户名不能为空');
-  } else if (!/[_a-zA-Z0-9]/g.test(username.trim())) {
-    errors.username.push('用户名格式不合法');
-  } else if (username.trim().length < 5 || username.trim().length > 30) {
-    errors.username.push('用户名长度为5-30之间');
-  } else if (connection.manager.find(User, { username })) {
-    errors.username.push('用户名已存在');
-  }
-  // 校验password
-  if (password === '') {
-    errors.password.push('密码不能为空')
-  } else if (password.length < 6) {
-    errors.password.push('密码长度不能小于6位')
-  }
-
-  if (password !== passwordConfirmation) {
-    errors.passwordConfirmation.push('密码不匹配')
-  }
-  const hasErrors = Object.values(errors).find(value => value.length > 0);
+  // 创建user对象
+  const user = new User();
+  user.username = username.trim();
+  user.password = password;
+  user.passwordConfirmation = passwordConfirmation;
+  user.passwordDigest = md5(password);
+  user.validate();
   res.setHeader('Content-Type', 'application/json;charset=utf-8');
-  if (hasErrors) {
+  if (user.hasErrors()) {
     res.statusCode = 422;
-    res.write(JSON.stringify(errors));
+    res.write(JSON.stringify(user.errors));
   } else {
-    // 连接数据库保存数据
-    const user = new User();
-    user.username = username.trim();
-    user.passwordDigest = md5(password);
+
     await connection.manager.save(user);
     res.statusCode = 200;
     const result = {
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,6 +1,7 @@
 import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, OneToMany, JoinColumn } from "typeorm";
 import { Post } from './Post'
 import { Comment } from './Comment'
+import { getDatabaseConnection } from './../../lib/getDatabaseConnection';
 @Entity('users')
 export class User {
   @PrimaryGeneratedColumn('increment')
@@ -17,4 +18,37 @@ export class User {
   posts: Post[];
   @OneToMany(type => Comment, comment => comment.user)
   comments: Comment[];
+  // 没有加装饰器表示不是字段不是数据库的，但是是类的
+  errors = {
+    username: [] as string[],
+    password: [] as string[],
+    passwordConfirmation: [] as string[]
+  };
+  password: string;
+  passwordConfirmation: string;
+  async validate() {
+    if (this.username.trim().length === 0) {
+      this.errors.username.push('用户名不能为空');
+    } else if (!/[_a-zA-Z0-9]/g.test(this.username.trim())) {
+      this.errors.username.push('用户名格式不合法');
+    } else if (this.username.trim().length < 5 || this.username.trim().length > 30) {
+      this.errors.username.push('用户名长度为5-30之间');
+    } else if ((await getDatabaseConnection()).manager.find(
+      User, { username: this.username })
+    ) {
+      this.errors.username.push('用户名已存在');
+    }
+    // 校验password
+    if (this.password === '') {
+      this.errors.password.push('密码不能为空')
+    } else if (this.password.length < 6) {
+      this.errors.password.push('密码长度不能小于6位')
+    }
+    if (this.password !== this.passwordConfirmation) {
+      this.errors.passwordConfirmation.push('密码不匹配')
+    }
+  };
+  hasErrors() {
+    return !!Object.values(this.errors).find(value => value.length > 0);
+  }
 }
