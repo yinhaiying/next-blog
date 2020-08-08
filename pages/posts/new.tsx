2294873be9b8b4effe50a34ad6b9adda31a1ceeb
yinhaiying <18840842571@163.com>
2020-08-08 09:49:10 +0800

    完善新增样式

--- pages/posts/new.tsx
+++ pages/posts/new.tsx
@@ -30,30 +30,74 @@ const New: NextPage = () => {
   }, [formData])
   return (
     <>
-      <h2>添加博客</h2>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={onSubmit} className="form-wrapper">
         <div>
-          <label >标题
-          <input type="text" value={formData.title} onChange={e => setFormData({
-            ...formData,
-            title: e.target.value
-          })} />
+          <label className="field-label">
+            <span className="label-title">标题：</span>
+            <input className="label-content" type="text" value={formData.title} onChange={e => setFormData({
+              ...formData,
+              title: e.target.value
+            })} />
           </label>
           {errors.title && errors.title.length > 0 ? <div>{errors.title.join(' ')}</div> : null}
         </div>
         <div>
-          <label >内容
-          <textarea value={formData.content} onChange={e => setFormData({
-            ...formData,
-            content: e.target.value
-          })} />
+          <label className="field-label">
+            <span className="label-title">内容：</span>
+            <textarea className="label-content" value={formData.content} onChange={e => setFormData({
+              ...formData,
+              content: e.target.value
+            })} />
           </label>
           {errors.content && errors.content.length > 0 ? <div>{errors.content.join(' ')}</div> : null}
         </div>
-        <div>
+        <div className="btn">
           <button type="submit">提交</button>
         </div>
       </form>
+      <style jsx>{
+
+        `
+        .form-wrapper{
+          padding:16px;
+        }
+        .field-label{
+          display:flex;
+          margin-bottom:20px;
+          margin-top:20px;
+          box-sizing:border-box;
+
+        }
+        .field-label > .label-title{
+          width:3.2em;
+          white-space:nowrap;
+        }
+        .field-label > .label-content{
+          flex:1;
+          border:1px solid rgb(169, 169, 169);
+        }
+        .field-label > textarea.label-content{
+          min-height:20em;
+        }
+        .btn{
+          display:flex;
+          justify-content:center;
+          align-items:center;
+
+        }
+        .btn button{
+          background:#007fff;
+          outline:none;
+          padding: 0.3em 1em;
+          border:none;
+          border-radius:3px;
+          color:#fff;
+          cursor:pointer;
+        }
+
+          `
+
+      }</style>
     </>
   )
 }
