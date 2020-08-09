272a3045a4c6e1271c02b89828bea311774af4d5
yinhaiying <18840842571@163.com>
2020-08-09 19:40:31 +0800

    测试4

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,5 +4,7 @@ docker start efe2 &&
   yarn install --production=false &&
   yarn build &&
   docker build -t haiying/node-web-app . &&
-  docker run --name  app --network=host -p 3000:3000 -d haiying/node-web-app &&
+  docker kill /app &&
+  docker rm /app &&
+  docker run --name app --network=host -p 3000:3000 -d haiying/node-web-app &&
   echo 'OK'
